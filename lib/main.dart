289e7fc915b26eff3b80e1a6d7f81a23236d84f2
Garri <3434455@mail.ru>
2023-05-25 10:37:57 +0500

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +32,185 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late VideoPlayerController _videoPlayerController;
+  Duration? _position = Duration.zero;
+  Duration? _duration = Duration.zero;
+  double _progress = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.network(
+      'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+    );
+    _videoPlayerController.addListener(() {
+      setState(() {});
     });
+    // _videoPlayerController.setLooping(true);
+    _videoPlayerController.initialize().then((_) {
+      _duration = _videoPlayerController.value.duration;
+      _videoPlayerController.addListener(_onControllerUpdate);
+      _videoPlayerController.play;
+      setState(() {});
+    });
+  }
+
+  void _onControllerUpdate() async {
+    _position = await _videoPlayerController.position;
+    final isPlaying = _videoPlayerController.value.isPlaying;
+    if (isPlaying) {
+      setState(() {
+        _progress = _position!.inMilliseconds.ceilToDouble() /
+            _duration!.inMilliseconds.ceilToDouble();
+      });
+    }
+  }
+
+  String getTimeString(Duration duration) {
+    int hours = duration.inHours;
+    int minutes = duration.inMinutes % 60;
+    int seconds = duration.inSeconds % 60;
+    String hoursString = '';
+    if (hours < 10 && hours > 0) {
+      hoursString = '0$hours:';
+    } else if (hours > 9) {
+      hoursString = '$hours:';
+    }
+    String minutesString;
+    if (minutes < 10 && minutes > 0) {
+      minutesString = '0$minutes:';
+    } else if (minutes > 9) {
+      minutesString = '$minutes:';
+    } else {
+      minutesString = '00:';
+    }
+    String secondsString = '';
+    if (seconds < 10 && seconds > 0) {
+      secondsString = '0$seconds';
+    } else if (seconds > 9) {
+      secondsString = '$seconds';
+    } else {
+      secondsString = '00';
+    }
+    return hoursString + minutesString + secondsString;
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SizedBox(
+        height: 200,
+        child: Stack(
+          children: [
+            VideoPlayer(_videoPlayerController),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: 100,
+                child: Column(
+                  children: [
+                    Slider(
+                      value: max(0, min(_progress * 100, 100)),
+                      min: 0,
+                      max: 100,
+                      divisions: 100,
+                      onChanged: (value) {
+                        setState(() {
+                          _progress = value * 0.01;
+                        });
+                      },
+                      onChangeStart: (_) {
+                        _videoPlayerController.pause();
+                      },
+                      onChangeEnd: (value) {
+                        final Duration duration =
+                            _videoPlayerController.value.duration;
+                        double newValue = max(0, min(value, 99)) * 0.01;
+                        int millis =
+                            (duration.inMilliseconds * newValue).toInt();
+                        _videoPlayerController
+                            .seekTo(Duration(milliseconds: millis));
+                        // _videoPlayerController.play();
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        children: [
+                          Text(
+                            getTimeString(_position!),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                          const Spacer(),
+                          Text(
+                            getTimeString(_duration!),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                bottom: 16,
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _videoPlayerController.seekTo(
+                            Duration(seconds: _position!.inSeconds - 10));
+                      },
+                      icon: const Icon(Icons.replay_10),
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        _videoPlayerController.value.isPlaying
+                            ? _videoPlayerController.pause()
+                            : _videoPlayerController.play();
+                      },
+                      icon: _videoPlayerController.value.isPlaying
+                          ? const Icon(Icons.pause)
+                          : const Icon(Icons.play_arrow),
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        _videoPlayerController.seekTo(
+                            Duration(seconds: _position!.inSeconds + 10));
+                      },
+                      icon: const Icon(Icons.forward_10),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
